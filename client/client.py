1a92004abcd4b47aa9364e630a2b51ab3aa35e1a
gg147 <gg147@duke.edu>
2022-12-07 21:50:31 -0500

    socket stuck

--- client/client.py
+++ client/client.py
@@ -1,54 +1,79 @@
 import socket 
 import secrets
 import hmac, hashlib
-from common.connection import *
+import bcrypt
+from connection import *
 
 class Client:
 
     def __init__(self):
         self.sock = socket.socket()
-        server = input("Enter server name: ")
-        self.sock.connect((server, 1234))
-        self.conn = Connect(self.sock)
+        while True:
+            server = input("Enter server name: ")
+            try:
+                self.sock.connect((server, 12345))
+                self.conn = Connect(self.sock)
+                break
+            except:
+                print("Connection to server " + server + " failed. Please try again.\n")
+                continue
         self.auth()
 
     def auth(self):
         option = 0
-        while option != 1 or option != 2: 
-            option = input("Please Enter:\n(1) for Login\n(2) for Register\n")
-        
+        while True: 
+            try:
+                option = int(input("Please Enter:\n(1) for Login\n(2) for Register\n"))
+                if option == 1 or option == 2:
+                    break
+            except:
+                print("\nWrong input type. Required int, Please try again\n") 
+               
         if option == 1:
-            self.conn.sendData("login")
+            self.conn.sendNonByte("login")
             self.Login()
         else:
-            self.conn.sendData("register")
+            self.conn.sendNonByte("register")
             self.Register()
     
     def getDetails(self):
         username = input("Enter Username: ")
         password = input("Enter Password: ")
-        self.conn.sendData(username)
+        self.conn.sendNonByte(username)
         return password
     
     def Register(self):
         out = False
         while out != True:
             password = self.getDetails()
-            self.conn.sendData(password)
+            self.conn.sendNonByte(password)
             out = self.conn.recvData()
+            if out == "False":
+                print("Register failed, Please try again\n")
     
     def Login(self):
         out = False
         while out != True:
             password = self.getDetails()
-            self.conn.sendData(secrets.token_urlsafe())
+            userExist = self.conn.recvData()
+            if userExist == "False":
+                print("username doesn't exist, Please try again\n")
+                continue
+
+            self.conn.sendNonByte(secrets.token_urlsafe()) 
             salt = self.conn.recvData()
             nonce = self.conn.recvData()
             passwd = bcrypt.hashpw(str(password).encode('utf-8'), salt.encode('utf-8')).decode()
             h = hmac.new(nonce, passwd, hashlib.sha256)
             proof = h.hexdigest().decode()
-            self.conn.sendData(proof)
+            self.conn.sendNonByte(proof)
+
             out = self.conn.recvData()
+            if out == "False":
+                print("Login failed, Please try again\n")
 
     def __del__(self):
         self.sock.close()
+
+if __name__ == "__main__":
+    c = Client()
--- server/connect.py
+++ server/connect.py
@@ -12,23 +12,25 @@ class serverAuth:
     def register(self):
         username = self.conn.recvData()
         password = self.conn.recvData()
-        out = self.auth.register(username, password)
-        self.conn.sendData(str(out))
+        out = self.auth.DBregister(username, password)
+        self.conn.sendNonByte(str(out))
         return out
 
     def login(self):
         username = self.conn.recvData()
+        salt = self.auth.getSalt(username)
+        if salt == None:
+            self.conn.sendNonByte("False")
+            return False
+        self.conn.sendNonByte("True")
+
         clientNonce = self.conn.recvData()
         serverNonce = secrets.token_urlsafe()
-        Nonce = clientNonce + serverNonce
-        salt = self.auth.getSalt(username)
-
-        if salt == None:
-            return False
-        
-        self.conn.sendData(salt)
-        self.conn.sendData(Nonce)
+        Nonce = clientNonce + serverNonce        
+        self.conn.sendNonByte(salt)
+        self.conn.sendNonByte(Nonce)
         proof = self.conn.recvData()
-        out = self.auth.login(username, proof, Nonce)
-        self.conn.sendData(str(out))
+        out = self.auth.DBlogin(username, proof, Nonce)
+        
+        self.conn.sendNonByte(str(out))
         return out
